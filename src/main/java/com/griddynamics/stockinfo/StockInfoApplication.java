a87be4e5d7da0059641eee988f70b953a1b9acc3
spezdmtest <spezd.m.@gmail.com>
2023-11-13 16:34:58 +0200

    Async API Data Manager

---
+++ src/main/java/com/griddynamics/stockinfo/StockInfoApplication.java
@@ -0,0 +1,13 @@
+package com.griddynamics.stockinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/griddynamics/stockinfo/StockinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griddynamics.stockinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
